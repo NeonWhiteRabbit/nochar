c5626e5edfd8f24f63769ccf79a5861f67b97dd4
̵͎̲̞̣̥͘N̛͚̟͉̭̘̘̯̙͈̫̹̜͟͢ͅȩ̛̛̣̰͓̻͎͡ͅo͈̙̦̖̹̦̙͝͞n̴̫̘͈͈͈̳̩͢ ̰͟W̨̛̺̪̱̼̝̙̲̰͚̗͓͝h̢̟̲͠҉͏̫̼̜͉̭i̵̙͙̙̥̰̯͎̘͈̤̩̝̣ṱ̛͓̘̯̙͈̫̹̜̕͢ͅȩ̣̰͓̻͎͡ ̰͖̯͕̥̱̲̰̳͟Ŗ̜̯͟͡a͏́͏̴͖͍̞̥͝͏̬͙͕ͅb̴͎͚̤̩̪͎͕̖͚͝͏̬͙͕ͅb͎͚̤̩̪͎͕̖͚҉͏̫̼̜͉̭i̵̙͙̙̥̰̯͎̘͈̤̩̝̣ṱ͓̕ <97998625+NeonWhiteRabbit@users.noreply.github.com>
2024-07-16 16:59:11 -0500

    Update nochar.sh
    takes arguments

--- nochar.sh
+++ nochar.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
 
-echo "What is the domain file?"
-read dom
+while getopts ":f:o:" o; do
+    case "${o}" in
+        f)
+            FileName=${OPTARG}
+            ;;
+        o)
+            OutputFile=${OPTARG}
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -z "${s}" ] || [ -z "${p}" ]; then
+    echo "The syntax is ./nochar.sh -f <domainslist.txt> -o <results.txt>"
+fi
+
+echo "FileName where domains are is set to ${FileName}"
+echo "Your results will be in a file called ${OutputFile}"
+
 touch output.txt
 
-for i in $(cat "$dom"); do
+for i in $(cat "$FileName"); do
     content=$(curl --head --max-time 2 "{$i}")
     echo "<-------------------->" >> output.txt
     echo "$content" >> output.txt
@@ -16,7 +33,7 @@ cat output.txt | grep "Location:" >> output1.txt
 cat output.txt | grep "Content-Type:" >> output2.txt
 cat output.txt | grep "<-------------------->" >> output3.txt
 
-paste -d'\n' code1.txt output1.txt output2.txt output3.txt > results.txt
+paste -d'\n' code1.txt output1.txt output2.txt output3.txt > $OutputFile
 
 rm output1.txt output2.txt output3.txt code.txt output.txt code1.txt
 
@@ -30,29 +47,29 @@ echo "--------------------------------------------------------------------------
 echo "-----------------------------------------------------------------------------------"
 echo "======================= RESPONSE CODES: 200 RANGE ================================="
 echo "-----------------------------------------------------------------------------------"
-cat results.txt | grep -C 3 "200"
-cat results.txt | grep -C 3 "201"
-cat results.txt | grep -C 3 "202"
-cat results.txt | grep -C 3 "203"
-cat results.txt | grep -C 3 "204"
-cat results.txt | grep -C 3 "205"
-cat results.txt | grep -C 3 "206"
-cat results.txt | grep -C 3 "207"
-cat results.txt | grep -C 3 "208"
-cat results.txt | grep -C 3 "226"
+cat $OutputFile | grep -C 3 "200"
+cat $OutputFile | grep -C 3 "201"
+cat $OutputFile | grep -C 3 "202"
+cat $OutputFile | grep -C 3 "203"
+cat $OutputFile | grep -C 3 "204"
+cat $OutputFile | grep -C 3 "205"
+cat $OutputFile | grep -C 3 "206"
+cat $OutputFile | grep -C 3 "207"
+cat $OutputFile | grep -C 3 "208"
+cat $OutputFile | grep -C 3 "226"
 echo "-----------------------------------------------------------------------------------"
 echo "-----------------------------------------------------------------------------------"
 echo "======================= RESPONSE CODES: 300 RANGE ================================="
 echo "-----------------------------------------------------------------------------------"
-cat results.txt | grep -C 3 "300"
-cat results.txt | grep -C 3 "301"
-cat results.txt | grep -C 3 "302"
-cat results.txt | grep -C 3 "303"
-cat results.txt | grep -C 3 "304"
-cat results.txt | grep -C 3 "305"
-cat results.txt | grep -C 3 "306"
-cat results.txt | grep -C 3 "307"
-cat results.txt | grep -C 3 "308"
+cat $OutputFile | grep -C 3 "300"
+cat $OutputFile | grep -C 3 "301"
+cat $OutputFile | grep -C 3 "302"
+cat $OutputFile | grep -C 3 "303"
+cat $OutputFile | grep -C 3 "304"
+cat $OutputFile | grep -C 3 "305"
+cat $OutputFile | grep -C 3 "306"
+cat $OutputFile | grep -C 3 "307"
+cat $OutputFile | grep -C 3 "308"
 echo "-----------------------------------------------------------------------------------"
-echo "------------ ALL OTHER RESPONSE CODES AVAILABLE IN results.txt --------------------"
+echo "------------ ALL OTHER RESPONSE CODES AVAILABLE IN $OutputFile --------------------"
 echo "-----------------------------------------------------------------------------------"
